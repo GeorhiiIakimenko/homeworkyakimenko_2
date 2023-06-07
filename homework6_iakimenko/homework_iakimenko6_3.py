de0e7120fb4e712dccf94ea4cdd6b4637456ad64
GeorhiiIakimenko <133113487+GeorhiiIakimenko@users.noreply.github.com>
2023-06-07 20:42:01 +0300

    Add files via upload

--- homework6_iakimenko/homework_iakimenko6_3.py
+++ homework6_iakimenko/homework_iakimenko6_3.py
@@ -1,3 +1,12 @@
+def get_age_word(age):
+    if age % 10 == 1 and age % 100 != 11:
+        return "рік"
+    elif age % 10 in [2, 3, 4] and age % 100 not in [12, 13, 14]:
+        return "роки"
+    else:
+        return "років"
+
+
 def cinema_cashier():
     age = input("Введіть свій вік: ")
 
@@ -11,27 +20,20 @@ def cinema_cashier():
         return
 
     age = int(age)
-
-    def get_age_word(age):
-        if age % 10 == 1 and 11 != age % 100:
-            return "рік"
-        elif age % 10 in [2, 3, 4] and age % 100 not in [12, 13, 14]:
-            return "роки"
-        else:
-            return "років"
+    age_word = get_age_word(age)  # Передайте аргумент 'age' при виклику функції get_age_word
 
     if age == 0:
         print("Некоректне число (Вік не може бути 0).")
     elif age < 7:
-        print(f"Тобі ж {age} {get_age_word(age)}! Де твої батьки?")
+        print(f"Тобі ж {age} {age_word}! Де твої батьки?")
     elif age < 16:
-        print(f"Тобі лише {age} {get_age_word(age)}, а це фільм для дорослих!")
+        print(f"Тобі лише {age} {age_word}, а це фільм для дорослих!")
     elif age > 65:
-        print(f"Вам {age} {get_age_word(age)}? Покажіть пенсійне посвідчення!")
+        print(f"Вам {age} {age_word}? Покажіть пенсійне посвідчення!")
     elif "7" in str(age):
-        print(f"Вам {age} {get_age_word(age)}, вам пощастить!")
+        print(f"Вам {age} {age_word}, вам пощастить!")
     else:
-        print(f"Незважаючи на те, що вам {age} {get_age_word(age)}, білетів всеодно немає!")
+        print(f"Незважаючи на те, що вам {age} {age_word}, білетів всеодно немає!")
 
 
 cinema_cashier()
